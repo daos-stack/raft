cd8c9179ca6910d055f4f2014789e5452807432f
Omkar Kulkarni <omkar.kulkarni@intel.com>
2018-06-28 14:30:55 -0700

    DAOS-1046 log: Support batch popping
    Code Review 02: Remove redundant 'back' field from log.
    
    Signed-off-by: Omkar Kulkarni <omkar.kulkarni@intel.com>

--- src/raft_log.c
+++ src/raft_log.c
@@ -28,7 +28,7 @@ typedef struct
     int count;
 
     /* position of the queue */
-    int front, back;
+    int front;
 
     /* we compact the log, and thus need to increment the Base Log Index */
     int base;
@@ -39,12 +39,6 @@ typedef struct
     raft_cbs_t *cb;
     void* raft;
 } log_private_t;
-
-static int mod(int a, int b)
-{
-    int r = a % b;
-    return r < 0 ? r + b : r;
-}
 
 static int __ensurecapacity(log_private_t * me, int n)
 {
@@ -61,16 +55,18 @@ static int __ensurecapacity(log_private_t * me, int n)
         return RAFT_ERR_NOMEM;
 
 
-    if (me->front < me->back)
+    if (me->front + me->count <= me->size)
     {
         memcpy(&temp[0], &me->entries[me->front],
-               sizeof(raft_entry_t) * (me->back - me->front));
+               me->count * sizeof(raft_entry_t));
     }
     else if (me->count > 0)
     {
-        int i = me->size - me->front;
-        memcpy(&temp[0], &me->entries[me->front], i * sizeof(raft_entry_t));
-        memcpy(&temp[i], &me->entries[0], me->back * sizeof(raft_entry_t));
+        int index = me->size - me->front;
+        memcpy(&temp[0], &me->entries[me->front],
+               index * sizeof(raft_entry_t));
+        memcpy(&temp[index], &me->entries[0],
+               (me->count - index) * sizeof(raft_entry_t));
     }
 
     /* clean up old entries */
@@ -79,7 +75,6 @@ static int __ensurecapacity(log_private_t * me, int n)
     me->size = newsize;
     me->entries = temp;
     me->front = 0;
-    me->back = me->count;
     return 0;
 }
 
@@ -140,7 +135,6 @@ void log_clear(log_t* me_)
 {
     log_private_t* me = (log_private_t*)me_;
     me->count = 0;
-    me->back = 0;
     me->front = 0;
     me->base = 0;
 }
@@ -212,9 +206,10 @@ int log_append(log_t* me_, raft_entry_t* ety, int *n)
 
     for (i = 0; i < *n; )
     {
-        raft_entry_t *ptr = &me->entries[me->back];
         int idx = me->base + me->count + 1;
         int k = batch_up(me, idx, *n - i);
+        int start = subscript(me, me->base + me->count + 1);
+        raft_entry_t *ptr = &me->entries[start];
         int batch_size = k;
         memcpy(ptr, &ety[i], k * sizeof(raft_entry_t));
         if (me->cb && me->cb->log_offer)
@@ -223,7 +218,6 @@ int log_append(log_t* me_, raft_entry_t* ety, int *n)
         if (k > 0)
         {
             me->count += k;
-            me->back = mod(me->back + k, me->size);
             i += k;
             raft_offer_log(me->raft, ptr, k, idx);
         }
@@ -248,7 +242,8 @@ int log_delete(log_t* me_, int idx)
     {
         int k = batch_down(me, me->base + me->count,
                                me->base + me->count - idx + 1);
-        raft_entry_t *ptr = &me->entries[me->back - 1 - k];
+        int start = subscript(me, me->base + me->count - k + 1);
+        raft_entry_t *ptr = &me->entries[start];
         int batch_size = k;
         if (me->cb && me->cb->log_pop)
             e = me->cb->log_pop(me->raft, raft_get_udata(me->raft),
@@ -256,7 +251,6 @@ int log_delete(log_t* me_, int idx)
         if (k > 0)
         {
             raft_pop_log(me->raft, ptr, k, idx);
-            me->back = mod(me->back - k, me->size);
             me->count -= k;
         }
         if (0 != e)
@@ -283,7 +277,8 @@ int log_poll(log_t* me_, int idx)
                                  &me->entries[me->front], me->base + 1, &k);
         if (k > 0)
         {
-            me->front = mod(me->front + k, me->size);
+            me->front += k;
+            me->front %= me->size;
             me->count -= k;
             me->base += k;
         }
@@ -301,11 +296,7 @@ raft_entry_t *log_peektail(log_t * me_)
 
     if (0 == me->count)
         return NULL;
-
-    if (0 == me->back)
-        return &me->entries[me->size - 1];
-    else
-        return &me->entries[me->back - 1];
+    return &me->entries[subscript(me, me->base + me->count)];
 }
 
 void log_empty(log_t * me_)
@@ -313,7 +304,6 @@ void log_empty(log_t * me_)
     log_private_t* me = (log_private_t*)me_;
 
     me->front = 0;
-    me->back = 0;
     me->count = 0;
 }
 
